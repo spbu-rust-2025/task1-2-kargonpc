94f9c4830f06a42d98dc1a7cd65b60c95ec7f424
kargonpc <idprokopev@gmail.com>
2025-10-22 00:17:02 +0300

    done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
+use std::io;
 
+fn main() {
+    let mut s = 0;
+    loop {
+        let mut l = String::new();
+        
+        io::stdin().read_line(&mut l).unwrap();
+        let l = l.trim();
+
+        if l == "-1" {
+            break; 
+        }
+
+        match l.parse::<i32>() {
+            Ok(n) if n > 0 => s += n,
+            _ => { println!("NaN"); return; }
+        }
+    }
+    println!("{}", s);
 }
